756a2d40eb3d90110abac4988689f91deb2f1f3b
Bew <Bew@sysops-MBP.broadband>
2020-12-01 23:00:27 +0000

    read a csv file and display the winning match

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('he he');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+// console.log(matches);
+let manUniteWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUniteWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUniteWins++;
+  }
+}
+
+console.log(`Man United won ${manUniteWins} games`);
